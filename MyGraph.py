f35739de20a0cf702d83020fe978e851c7107f04
Sean Burke <sean_burke1@baylor.edu>
2024-09-25 10:16:16 -0500

    Message

--- MyGraph.py
+++ MyGraph.py
@@ -8,3 +8,13 @@ plt.show()
 print("Hello there!")
 
 print("Hi again!")
+
+#To install virtual environment in terminal:
+# Create Virtual Environment
+# py -3 -m venv myvenv (or whatever you want to call it)
+
+# Activate Virtual Environment
+# \myvenv\Scripts\activate (tab to autocomplete)
+
+#Install the third party libraries
+# Pip3 install matplotlib 
